678dc353f635526629bf751cc4845d59a917d134
t-leonhardt <tim.leonhardt30@gmail.com>
2025-03-16 13:45:36 -0500

    Implemented HelperConfig for Anvil

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,8 +1,14 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2Mock} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
 abstract contract CodeConstants{
+    // Values for Mock
+    uint96 public MOCK_BASE_FEE = 0.25 ether;
+    uint96 public MOCK_GAS_PRICE_LINK = 1e9;
+    int256 public MOCK_WEI_PER_UNIT_LINK = 4e16; 
+    
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
@@ -51,5 +57,23 @@ contract HelperConfig is CodeConstants, Script{
         if(localNetworkConfig.vrfCoordinator != address(0)){
             return localNetworkConfig;
         }
+
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinatorMock = new VRFCoordinatorV2Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK
+        );
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: address(vrfCoordinatorMock),
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae, // does not matter if changed
+            callbackGasLimit: 50000,
+            subscriptionID: 0 // fix later 
+        });
+
+        return localNetworkConfig;
     }
 }
